ec67ec512d10f8fccd4fe7f87d6094af17ba9969
Alan Charles <alanjcharles14@gmail.com>
2018-05-25 12:10:11 -0600

    Add initial setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+
+import { BrowserRouter as Router} from 'react-router-dom';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../src/reducers/index';
+import App from './containers/App/App';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+render(
+  <Provider store={store} >
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
