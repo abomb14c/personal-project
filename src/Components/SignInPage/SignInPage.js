3baaae0c7f6b7a67984adeabbe1a740ca73106ea
Alan Charles <alanjcharles14@gmail.com>
2018-05-28 14:18:02 -0600

    Add structure for signIn file

--- src/Components/SignInPage/SignInPage.js
+++ src/Components/SignInPage/SignInPage.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { SignUpLink } from '../SignUpPage/SignUpPage'
+import { auth } from '../../firebase/index'
+import * as routes from '../../constants/routes'
 
-const SignInPage = () =>
+const SignInPage = ({ history }) =>
   <div>
-    <h1>Sign In Page</h1>
+    <h1>SignIn</h1>
+    <SignInForm history={history} />
+    <SignUpLink />
   </div>
+
+  const byPropKey = (propertyName, value) => () => ({
+    [propertyName]: value
+  });
+
+  const INITIAL_STATE = {
+    email: "",
+    password: "",
+    error: null 
+  };
 
 export default SignInPage;
