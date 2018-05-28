2a234425b7f260195f75d09f7f72f73a30150096
Alan Charles <alanjcharles14@gmail.com>
2018-05-28 13:03:37 -0600

    Add files for login pages

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
 import LeagueContainer from '../LeagueContainer/LeagueContainer'
+
 import Navigation from '../../Components/Nav/Navigation'
+import LandingPage from '../../Components/LandingPage/LandingPage'
+import SignUpPage from '../../Components/SignUpPage/SignUpPage'
+import SignInPage from '../../Components/SignInPage/SignInPage'
+import PasswordForgetPage from '../../Components/PasswordForget/PasswordForget'
+import HomePage from '../../Components/Home/Home'
+import AccountPage from '../../Components/AccountPage/AccountPage'
 
 import * as routes from '../../constants/routes'
+
 
 class App extends Component {
   
