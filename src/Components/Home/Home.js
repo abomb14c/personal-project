7d70601d9569741692f8dc98f7d964dcf4c0916e
Alan Charles <alanjcharles14@gmail.com>
2018-05-28 18:03:29 -0600

    add authorization for homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import withAuthorization from '../../WithAuthorization/WithAuthorization'
+
 
 const Home = () =>
   <div>
     <h1>Home</h1>
+    <p>The Home Page is accessible by every signed in user.</p>
   </div>
 
-export default Home;
+
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(Home);
